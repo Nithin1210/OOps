f5d0fd5734408194f071f153ca214ff2dbcdaf42
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-19 22:03:21 +0530

    [Nithin] Add : Updation of Inventory_Management

---
+++ OOPs/InventoryManagement/InventoryManagementOperation.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using OOPs.DataInventoryManagement;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace OOPs.InventoryManagement
+{
+    public class InventoryManagementOperations
+    {
+        InventoryManagementDetails list;
+        public void ReadInventoryJson(string filepath)
+        {
+            var json = File.ReadAllText(filepath);
+            list = JsonConvert.DeserializeObject<InventoryManagementDetails>(json);
+            Display(list.RiceList);
+            Display(list.WheatList);
+            Display(list.PulsesList);
+        }
+        public void AddInventoryManagement(string objectName)
+        {
+            Console.WriteLine("here we go my dear Friend we can make you proud and Brave ");
+            if (objectName.ToLower().Equals("rice"))
+            {
+                Console.WriteLine("Enter the Details : Name,Weight,PricePerKg");
+                InventoryDetails details = new InventoryDetails()
+                {
+                    Name = Console.ReadLine(),
+                    Weight = Convert.ToInt32(Console.ReadLine()),
+                    PricePerKg = Convert.ToInt32(Console.ReadLine())
+                };
+                list.RiceList.Add(details);
+            }
+            if (objectName.ToLower().Equals("wheat"))
+            {
+                Console.WriteLine("Enter the Details : Name,Weignt,PricePerKg");
+                InventoryDetails details = new InventoryDetails()
+                {
+                    Name = Console.ReadLine(),
+                    Weight = Convert.ToInt32(Console.ReadLine()),
+                    PricePerKg = Convert.ToInt32(Console.ReadLine())
+                };
+                list.WheatList.Add(details);
+            }
+            if (objectName.ToLower().Equals("pulses"))
+            {
+                Console.WriteLine("Enter the Details : Name,Weignt,PricePerKg");
+                InventoryDetails details = new InventoryDetails()
+                {
+                    Name = Console.ReadLine(),
+                    Weight = Convert.ToInt32(Console.ReadLine()),
+                    PricePerKg = Convert.ToInt32(Console.ReadLine())
+                };
+                list.PulsesList.Add(details);
+            }
+        }
+            public void WriteToJsonFile(string filepath)
+            {
+
+                var json = JsonConvert.SerializeObject(list);
+                File.WriteAllText(filepath, json);
+            }
+
+
+
+            public void Display(List<InventoryDetails> list)
+            {
+                foreach (var data in list)
+                {
+                    Console.WriteLine(data.Name + " " + data.Weight + " " + data.PricePerKg);
+                
+            }
+            }
+    }
+}
+
+
+
---
+++ OOPs/Program.cs
@@ -0,0 +1,48 @@
+﻿using OOPs.DataInventoryManagement;
+using System;
+using System.Threading.Channels;
+
+namespace OOPs.InventoryManagement
+{
+    public class program
+    {
+        static string inventory_filePath = @"E:\Json\OOPs\InventoryManagement\InventoryManagementData.json";
+        public static void Main(string[] args)
+        {
+            //InventoryDetailsOperation details = new InventoryDetailsOperation();
+            //details.ReadInventoryJson(inventory_filePath);
+
+            //InventoryManagementOperations details = new InventoryManagementOperations();
+            //details.ReadInventoryJson(inventory_filePath);
+
+            bool flag = true;
+            InventoryManagementOperations details = new InventoryManagementOperations();
+            while (flag)
+            {
+                Console.WriteLine("Enter the Value :\n1.Display \n2.Add \n3.Bye");
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        details.ReadInventoryJson(inventory_filePath);
+                        break;
+                    case 2:
+                        Console.WriteLine("Add the Details (rice or wheat or pulses) : Name,Weight,PricePerKg");
+                        string name = Console.ReadLine();
+                        details.AddInventoryManagement(name);
+                        details.WriteToJsonFile(inventory_filePath);
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+
+
+
+
+                }
+
+            }
+        }
+
+    }
+}
